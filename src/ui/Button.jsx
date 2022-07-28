27c680e592f83abc10f5b777bcf06e2e8cdbbe96
Sat Naing <satnaingdev@gmail.com>
2022-07-28 12:12:08 +0630

    add aria-labels in buttons

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,6 +4,7 @@ const Button = ({ children, cname, handleClick, val, title }) => {
       onClick={handleClick}
       value={val}
       title={title}
+      aria-label={title}
       className={`bg-primary hover:bg-primary-100 shadow-lg text-secondary focus:outline-none focus:ring-1 focus:ring-primary-100 focus:ring-opacity-50 w-40 h-10 text-2xl rounded-sm ${cname}`}
     >
       {children}
--- src/ui/RoundedButton.jsx
+++ src/ui/RoundedButton.jsx
@@ -3,6 +3,7 @@ const RoundedButton = ({ children, handleClick, title }) => {
     <button
       onClick={handleClick}
       title={title}
+      aria-label={title}
       className={`bg-primary hover:bg-primary-100 shadow-lg text-secondary focus:outline-none focus:ring-1 focus:ring-primary-100 focus:ring-opacity-50  invisible sm:visible absolute right-0 w-8 h-8 rounded-full`}
     >
       {children}
